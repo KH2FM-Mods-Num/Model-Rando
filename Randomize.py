edff9497f93b8f7042322fd986a89f286955fce7
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-04-25 15:52:48 +0700

    Revamped randomization method
    Uses less files & less chance of vanilla files appearing

--- Randomize.py
+++ Randomize.py
@@ -5,40 +5,50 @@ import random
 
 #Functions to write stuff
 def writemodel(old,new):
-    #MDLX (Avoid conflict with Wisdom & Limit Form)
-    f.write('- name: obj/'+old+'.mdlx\n  method: binarc\n  source:\n')
-    subfile = old[:4].lower()
-    if objtype == 'Enemy':
-        if os.path.isfile('obj/'+new+'.model'): #Changed Enemy Model
-            f.write('  - name: '+subfile+'\n    type: model\n    method: copy\n    source:\n'+
-                    '    - name: obj/'+new+'.model\n') #Model
-        f.write('  - name: tim_\n    type: modeltexture\n    method: copy\n    source:\n'+
-                '    - name: obj/'+new+'.tim\n')
-        if os.path.isfile('obj/'+new+'.a.fm'): #Special Sound Effects
-            f.write('- name: obj/'+old+'.a.fm\n  method: copy\n  source:\n'+
-                    '  - name: obj/'+new+'.a.fm\n')
-        return
-    if '_PLAYER' in old:
-        subfile = 'p_ex'
-    f.write('  - name: '+subfile+'\n    type: model\n    method: copy\n    source:\n'+
-            '    - name: obj/'+new+'.model\n') #Model
-    f.write('  - name: tim_\n    type: modeltexture\n    method: copy\n    source:\n'+
-            '    - name: obj/'+new+'.tim\n') #Texture
-    
+    old = 'obj/' + old
+    new = 'obj/' + new
     #A.FM
-    if objtype == 'Party':
-        if os.path.isfile('obj/'+new+'.a.fm'): #Special Sound Effects
-            f.write('- name: obj/'+old+'.a.fm\n  method: copy\n  source:\n'+
-                    '  - name: obj/'+new+'.a.fm\n')
-            return
-        f.write('- name: obj/'+old+'.a.fm\n  method: binarc\n  source:\n')
-        f.write('  - name: face\n    type: imgd\n    method: copy\n    source:\n'+
-                '    - name: obj/'+new+'.imd\n') #Portrait
-        f.write('  - name: face\n    type: seqd\n    method: copy\n    source:\n'+
-                '    - name: obj/'+new+'.sqd\n') #Portrait Location
-    elif objtype == 'Weapon':
-        f.write('- name: obj/'+old+'.a.fm\n  method: copy\n  source:\n'+
-                '  - name: obj/'+new+'.a.fm\n') #Particle & Sound Effect
+    if os.path.isfile(old+'.fm'):
+        f.write('- name: '+old+'.a.fm\n')
+        f.write('  method: copy\n')
+        f.write('  source:\n')
+        f.write('  - name: '+new+'.a.fm\n')
+    elif os.path.isfile(old+'.imd') or os.path.isfile(old+'.sqd'):
+        f.write('- name: '+old+'.a.fm\n')
+        f.write('  method: binarc\n')
+        f.write('  source:\n')
+        if os.path.isfile(old+'.imd'):
+            f.write('  - name: face\n')
+            f.write('    type: imgd\n')
+            f.write('    method: copy\n')
+            f.write('    source:\n')
+            f.write('    - name: '+new+'.imd\n')
+        if os.path.isfile(old+'.sqd'):
+            f.write('  - name: face\n')
+            f.write('    type: seqd\n')
+            f.write('    method: copy\n')
+            f.write('    source:\n')
+            f.write('    - name: '+new+'.sqd\n')
+    #MDLX
+    if os.path.isfile(old+'.model') or os.path.isfile(old+'.tim'):
+        f.write('- name: '+old+'.mdlx\n')
+        f.write('  method: binarc\n')
+        f.write('  source:\n')
+        if os.path.isfile(old+'.model'):
+            subfile = old[:4].lower()
+            if '_PLAYER' in old:
+                subfile = 'p_ex'
+            f.write('  - name: '+subfile+'\n')
+            f.write('    type: model\n')
+            f.write('    method: copy\n')
+            f.write('    source:\n')
+            f.write('    - name: '+new+'.model\n')
+        if os.path.isfile(old+'.tim'):
+            f.write('  - name: tim_\n')
+            f.write('    type: modeltexture\n')
+            f.write('    method: copy\n')
+            f.write('    source:\n')
+            f.write('    - name: '+new+'.tim\n')
 
 def randomodel(oldmodel):
     global subfile
@@ -50,19 +60,22 @@ def randomodel(oldmodel):
 #Get KH2 model filenames
 currentDir = sys.argv[0].replace((sys.argv[0].split('\\')[-1]),'')
 objs = yaml.safe_load(open(currentDir+'modellist.yml'))
-objsextra = yaml.safe_load(open(currentDir+'modellistextra.yml'))
 
 #Write the mod.yml
 f = open(currentDir+'mod.yml','w',encoding='utf-8')
-f.write('description: Credits for PandaPyre, Shananas, and GeminiHero for providing their respective textures.\n')
+f.write('description: Credits for PandaPyre, Shananas, GeminiHero, and DA for providing their respective textures.\n')
 f.write('assets:\n')
 for objtype in objs:
     obj = objs[objtype]
-    for models in obj:
-        oldmodels = objs[objtype][models] + objsextra['Domain'][objtype][models]
-        newmodels = objs[objtype][models] + objsextra['Codomain'][objtype][models]
+    for models in obj.values():
+        oldmodels = models['Base'] + models['ExtraDomain']
+        newmodels = models['Base'] + models['ExtraCodomain']
+        while len(oldmodels) > len(newmodels):
+            newmodels *= 2
         random.shuffle(newmodels)
         for i in range(len(oldmodels)):
+            if oldmodels[i] == newmodels[i]:
+                pass
             writemodel(oldmodels[i],newmodels[i])
 
 f.close()
